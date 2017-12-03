62e88d6fdebef4a45bbd6822236504b09c696f84
dghamelin <ghamelin@fileshadow.com>
2017-12-02 18:30:13 -0700

    add routes to server

--- server.js
+++ server.js
@@ -11,7 +11,17 @@ app.get('/', function(req,res){
   res.send('home page');
 });
 
+app.get('/premise', function(req,res) {
+  res.send('premise page');
+});
+app.get('/sources', function(req,res){
+  res.send('sources page');
+});
+app.get('/contact', function(req,res){
+  res.send('contact page');
+})
+
 // start server
 app.listen(port, function(){
   console.log('app listening on http://localhost:'+port)
-})
+});
