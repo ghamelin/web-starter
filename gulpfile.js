de2f505ab7bc5ac9746431ac269a66b69bd69d6b
dghamelin <ghamelin@fileshadow.com>
2017-12-03 00:48:25 -0700

    add build tasks to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var plugs           = [shortcss, cssnext];
 var sassSrc = "src/scss/main.scss";
 var sassDest = "public/css/";
 
-
+// compile scss
 gulp.task('sass', function () {
   return gulp.src([sassSrc])
     .pipe(plugins.sourcemaps.init())
@@ -22,3 +22,28 @@ gulp.task('sass', function () {
     .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest(sassDest))
 })
+//process images
+gulp.task('img', function () {
+  return gulp.src(['src/img/*.+(png|jpg|gif|svg|ico)'])
+    .pipe(plugins.imagemin({
+      interlaced: true,
+      optimizationLevel: 10,
+      multipass: true,
+      use: [imageminMozjpeg()]
+    }))
+    .pipe(gulp.dest('public/img/'))
+});
+
+// clean the production folder
+gulp.task('clean', function () {
+  return del.sync('public/*')
+});
+
+// build site for production
+// cleans folder and recompiles all files into production folder
+gulp.task('build', function () {
+  runSequence('clean', ['img', 'sass']);
+});
+
+// sets up folders to watch for changes while making edits
+gulp.task('default', ['img', 'sass']);
